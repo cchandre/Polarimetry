68c12550b69a038841f0e23112e48fa48982de09
Cristel Chandre <cchandre@gmail.com>
2022-09-26 17:20:33 +0200

    Minor edits

--- registration_temp.m
+++ registration_temp.m
@@ -35,19 +35,19 @@ CellInd = ImCG{IndI,IndJ};
 Dark = mean(CellInd(CellInd~=0));
 
 Itot = sum((Stack.Values-Dark).*(Stack.Values>=Dark),3);
-figure(1), imagesc(imadjust(Itot/max(Itot(:)))), hold on
+%figure(1), imagesc(imadjust(Itot/max(Itot(:)))), hold on
 
-figure(2)
+%figure(2)
 for it = 1:2
-    subplot(2,1,it)
+    %subplot(2,1,it)
     vec = sum(Itot,it);
-    plot(vec,'linewidth',3)
-    xlim([1 info(1).Height])
+    %plot(vec,'linewidth',3)
+    %xlim([1 info(1).Height])
     [~,I] = min(vec(401:600));
     pos(it) = I+400;
 end
-figure(1), plot(xlim,[pos(2) pos(2)],'r','linewidth',3)
-plot([pos(1) pos(1)],ylim,'r','linewidth',3)
+%figure(1), plot(xlim,[pos(2) pos(2)],'r','linewidth',3)
+%plot([pos(1) pos(1)],ylim,'r','linewidth',3)
 
 %pos(1) = floor(Stack.Width/2);
 %pos(2) = floor(Stack.Height/2);
@@ -56,7 +56,7 @@ fixed = Itot(1:pos(2),1:pos(1));
 fixed = fixed/max(fixed(:));
 fixed = (fixed>=0.1).*fixed;
 
-moving = Itot(pos(2)+1:end,pos(1)+1:end);
+%moving = Itot(pos(2)+1:end,pos(1)+1:end);
 %moving = Itot(1:pos(1),pos(2)+1:end);
 moving = Itot(pos(1)+1:end,pos(2)+1:end);
 
@@ -67,7 +67,11 @@ figure, imagesc(fixed)
 figure, imagesc(moving)
 
 [optimizer,metric] = imregconfig('multimodal');
-optimizer.MaximumIterations = 200;
+
+optimizer.InitialRadius = 1e-3;
+optimizer.Epsilon = 1e-6;
+optimizer.GrowthFactor = 1.02;
+optimizer.MaximumIterations = 2000;
 
 %movingRegistered = imregister(moving,fixed,'translation',optimizer,metric);
 %movingRegistered = imregister(movingRegistered,fixed,'affine',optimizer,metric);
@@ -83,6 +87,8 @@ movingRegistered = imwarp(movingRegistered,tform_a,'OutputView',imref2d(size(fix
 
 
 figure, imshowpair(fixed,movingRegistered,'Scaling','joint')
+
+registrationEstimator(moving,fixed)
 
 
 
