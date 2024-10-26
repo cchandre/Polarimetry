12b1946fa7cda4aadde445f0c0193be02bf7cd6c
Cristel Chandre <cchandre@gmail.com>
2024-10-26 18:57:58 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,15 +1,14 @@
-import setuptools
+from setuptools import setup, find_packages
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
-setuptools.setup(
+setup(
     name='PyPOLAR',
     version='2.6.4',
     description='PyPOLAR is a Python-based app for analyzing polarization-resolved microscopy data to measure molecular orientation and order in biological samples',
     long_description=long_description,
     long_description_content_type="text/markdown",
-    packages=setuptools.find_packages(),
     url='https://www.fresnel.fr/polarimetry',
     classifiers=[
       'Programming Language :: Python :: 3',
@@ -18,27 +17,30 @@ setuptools.setup(
       'License :: OSI Approved :: BSD License',
       'Operating System :: OS Independent',
       'Topic :: Scientific/Engineering',
-	  'Topic :: Scientific/Engineering :: Bio-Informatics',
-	  'Topic :: Scientific/Engineering :: Visualization',
-	  'Topic :: Scientific/Engineering :: Image Processing'
+	    'Topic :: Scientific/Engineering :: Bio-Informatics',
+	    'Topic :: Scientific/Engineering :: Visualization',
+	    'Topic :: Scientific/Engineering :: Image Processing'
     ],
     python_requires='>=3.8',
     author='Cristel Chandre',
     author_email='cristel.chandre@cnrs.fr',
     license='BSD',
+    packages=find_packages(where="pypolar"),
     py_modules=['PyPOLAR', 'pypolar_classes', 'generate_json'],
     package_dir={'': 'pypolar'}, 
+    package_data={"icons": ["*.png", "*.icns", "*.ico"], "calibration": ["*.txt"], "diskcones": ["*.mat"]},
     install_requires=[
-		"colorcet==3.0.1",
-		"customtkinter==5.2.0",
-		"joblib==1.2.0",
-		"matplotlib==3.6.2",
-		"numpy==1.23.5",
-		"opencv-python==4.8.1.78",
-		"openpyxl==3.0.9",
-		"pillow==10.3.0",
-		"scikit-image==0.20.0",
-		"scipy==1.9.1",
-		"tksheet==6.2.5",
-		"tk==8.6.13"]
+		  "colorcet==3.0.1",
+		  "customtkinter==5.2.0",
+		  "joblib==1.2.0",
+		  "matplotlib==3.6.2",
+		  "numpy==1.23.5",
+		  "opencv-python==4.8.1.78",
+		  "openpyxl==3.0.9",
+		  "pillow==10.3.0",
+		  "scikit-image==0.20.0",
+		  "scipy==1.9.1",
+		  "tksheet==6.2.5",
+		  "tk==8.6.13"
+    ]
 )
